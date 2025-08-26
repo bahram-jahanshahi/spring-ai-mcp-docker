2630d96f375b681890a2be783b250bc3fe67da57
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2025-08-26 20:10:53 +0200

    Add application properties

---
+++ src/main/java/se/bahram/ai/spring_ai_mcp_docker/SpringAiMcpDockerApplication.java
@@ -0,0 +1,13 @@
+package se.bahram.ai.spring_ai_mcp_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMcpDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiMcpDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/bahram/ai/spring_ai_mcp_docker/SpringAiMcpDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.bahram.ai.spring_ai_mcp_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiMcpDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
